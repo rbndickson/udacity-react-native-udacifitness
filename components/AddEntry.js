1127d12a3318130148d0c9d3f758ab6762f8d97e
Robin Dickson <robindickson@gmail.com>
2017-10-09 21:38:30 +0200

    feat: Add reset button

--- components/AddEntry.js
+++ components/AddEntry.js
@@ -4,6 +4,8 @@ import { getMetricMetaInfo, timeToString } from '../utils/helpers';
 import UdaciSlider from './UdaciSlider';
 import UdaciSteppers from './UdaciSteppers';
 import DateHeader from './DateHeader';
+import { Ionicons } from '@expo/vector-icons';
+import TextButton from './TextButton';
 
 function SubmitBtn ({ onPress }) {
   return (
@@ -67,8 +69,28 @@ export default class AddEntry extends Component {
     // Save to db
     // Clear local notification
   }
+
+  reset = () => {
+    const key = timeToString();
+    // Update Redux
+    // Route to Home
+    // Update db
+  }
   render() {
     const metaInfo = getMetricMetaInfo();
+
+    if (this.props.alreadyLogged) {
+      return (
+        <View>
+          <Ionicons
+            name='ios-happy-outline'
+            size={100}
+          />
+          <Text>You already logged your information for today.</Text>
+          <TextButton onPress={this.reset}>Reset</TextButton>
+        </View>
+      )
+    }
 
     return (
       <View>
