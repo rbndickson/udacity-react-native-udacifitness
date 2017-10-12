28d86beed48de1acec03e7a529d1339c9ed3950d
Robin Dickson <robindickson@gmail.com>
2017-10-12 23:23:53 +0200

    feat: Add enable location services screen

--- components/Live.js
+++ components/Live.js
@@ -1,11 +1,16 @@
 import React from 'react';
-import { View, Text, ActivityIndicator } from 'react-native';
+import { View, Text, ActivityIndicator, TouchableOpacity, StyleSheet } from 'react-native';
+import { Foundation } from '@expo/vector-icons';
+import { purple, white } from '../utils/colors';
 
 export default class Live extends React.Component {
   state = {
     coords: null,
-    status: null,
+    status: 'undetermined',
     direction: ''
+  };
+  askPermission = () => {
+
   }
   render() {
     const { status, coords, direction } = this.state;
@@ -24,8 +29,14 @@ export default class Live extends React.Component {
 
     if (status === 'undetermined') {
       return (
-        <View>
-          <Text>Undetermined</Text>
+        <View style={styles.center}>
+          <Foundation name='alert' size={50} />
+          <Text>You need to enable location services for this feature</Text>
+          <TouchableOpacity onPress={this.askPermission} style={styles.button}>
+            <Text style={styles.buttonText}>
+              Enable
+            </Text>
+          </TouchableOpacity>
         </View>
       )
     }
@@ -38,3 +49,28 @@ export default class Live extends React.Component {
     )
   }
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'space-between'
+  },
+  center: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    marginLeft: 30,
+    marginRight: 30,
+  },
+    button: {
+    padding: 10,
+    backgroundColor: purple,
+    alignSelf: 'center',
+    borderRadius: 5,
+    margin: 20,
+  },
+    buttonText : {
+    color: white,
+    fontSize: 20,
+  }
+})
