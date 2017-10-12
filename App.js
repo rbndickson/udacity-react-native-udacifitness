7b809e6667474d93d0543dff265ff837bbe866ef
Robin Dickson <robindickson@gmail.com>
2017-10-12 23:12:00 +0200

    feat: Add live tab

--- App.js
+++ App.js
@@ -9,7 +9,8 @@ import { TabNavigator, StackNavigator } from 'react-navigation';
 import { purple, white } from './utils/colors';
 import { FontAwesome, Ionicons } from '@expo/vector-icons';
 import { Constants } from 'expo';
-import EntryDetail from './components/EntryDetail'
+import EntryDetail from './components/EntryDetail';
+import Live from './components/Live'
 
 function UdaciStatusBar ({ backgroundColor, ...props }) {
   return (
@@ -32,6 +33,13 @@ const Tabs = TabNavigator({
     navigationOptions: {
       tabBarLabel: 'AddEntry',
       tabBarIcon: ({ tintColor }) => <FontAwesome name='plus-square' size={30} color={tintColor} />
+    }
+  },
+  Live: {
+    screen: Live,
+    navigationOptions: {
+      tabBarLabel: 'Live',
+      tabBarIcon: ({ tintColor }) => <Ionicons name='ios-speedometer' size={30} color={tintColor} />
     }
   }
 }, {
