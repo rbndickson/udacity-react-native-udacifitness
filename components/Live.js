37786dc97ce5864e1bab222739cd79eaccc3afa7
Robin Dickson <robindickson@gmail.com>
2017-10-13 13:10:02 +0200

    feat: Add permission check for location
    Including asking for permission if it has not yet been granted.

--- components/Live.js
+++ components/Live.js
@@ -2,6 +2,8 @@ import React from 'react';
 import { View, Text, ActivityIndicator, TouchableOpacity, StyleSheet } from 'react-native';
 import { Foundation } from '@expo/vector-icons';
 import { purple, white } from '../utils/colors';
+import { Location, Permissions } from 'expo';
+import { calculateDirection } from '../utils/helpers';
 
 export default class Live extends React.Component {
   state = {
@@ -9,8 +11,47 @@ export default class Live extends React.Component {
     status: 'granted',
     direction: ''
   };
-  askPermission = () => {
+  componentDidMount () {
+    Permissions.getAsync(Permissions.LOCATION)
+      .then(({ status }) => {
+        if (status === 'granted') {
+          return this.setLocation();
+        }
 
+        this.setState(() => ({ status }))
+      })
+      .catch((error) => {
+        console.warn('Error getting Location permission: ', error)
+
+        this.setState(() => ({ status: 'undetermined' }))
+      })
+  }
+  askPermission = () => {
+    Permissions.askAsync(Permissions.LOCATION)
+      .then(({ status }) => {
+        if (status === 'granted') {
+          return this.setLocation();
+        }
+
+        this.setState(() => ({ status }))
+      })
+      .catch((error) => console.warn('Error asking locations permission: ', error))
+  }
+  setLocation = () => {
+    Location.watchPositionAsync({
+      enableHighAccuracy: true,
+      timeInterval: 1,
+      distanceInterval: 1,
+    }, ({ coords }) => {
+      const newDirection = calculateDirection(coords.heading);
+      const { direction } = this.state;
+
+      this.setState(() => ({
+        coords,
+        status: 'granted',
+        direction: newDirection
+      }))
+    })
   }
   render() {
     const { status, coords, direction } = this.state;
